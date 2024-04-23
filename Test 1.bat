97ea8ce05345361e72e1a38c36ae40ae92434375
Fybii <Fabian.Peifer23@gmail.com>
2024-04-23 22:02:42 +0200

    kjk

--- Test 1.bat
+++ Test 1.bat
@@ -1,3 +1,3 @@
 echo on
 cmd.exe /k
-set BOOST_INCLUDEDIR21=C:\sicherung\tools\MinGW\include && set BOOST_INCLUDEDIR=C:\sicherung\tools\MinGW\include && set PATH=%PATH%;C:\sicherung\tools\VSCode
+set BOOST_INCLUDEDIR21=C:\sicherung\tools\MinGW\include && set BOOST_INCLUDEDIR=C:\sicherung\tools\MinGW\include && set PATH=%PATH%;C:\DHBW Projekte\Web Engineering - Projekt Tutorium .docx
