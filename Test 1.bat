4e97bf8de4b4d4f5dd17848078a7ba8484386cdc
maxen <ironistus@gmail.com>
2024-04-23 19:51:42 +0200

    kjnön

--- Test 1.bat
+++ Test 1.bat
@@ -1,2 +1,3 @@
 @echo off
-cmd.exe /c exit
+ && set BOOST_INCLUDEDIR21=C:\sicherung\tools\MinGW\include && set PATH=%PATH%;C:\sicherung\tools\VSCode && set BOOST_INCLUDEDIR=C:\sicherung\tools\MinGW\include && cmd.exe /c exit
+stinkt && 
