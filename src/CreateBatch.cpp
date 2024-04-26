c78f4be3675ab0a1aeaf37a5d1db215edc621818
Fybii <Fabian.Peifer23@gmail.com>
2024-04-26 17:25:08 +0200

    sd

--- src/CreateBatch.cpp
+++ src/CreateBatch.cpp
@@ -14,14 +14,15 @@ void createBatchFile(JsonReader file)
         if (name[i] == '.') {
             std::string beforePoint = name.substr(0, i);
             fileName = beforePoint + ".bat";
-            std::ofstream batchFile(fileName, std::ios::binary);
             break;
         }
         else if ( i == nameLength - 1){
             fileName = name + ".bat";
-            std::ofstream batchFile(filename, std::ios::binary);
         }
     }
+
+    std::ofstream batchFile(fileName, std::ios::binary);
+
 
     // Überprüfen, ob die Datei erfolgreich geöffnet wurde
     if (!batchFile.is_open())
@@ -118,7 +119,7 @@ void createBatchFile(JsonReader file)
 
     if (!file.applicationValue.isNull())
     {
-        batchFile << " && start \"" + filename + "\" " << file.applicationValue.asString();
+        batchFile << " && start \"" + fileName + "\" " << file.applicationValue.asString();
     }
 
     batchFile << "\"\n@ECHO ON\r\n";
@@ -132,93 +133,3 @@ void createBatchFile(JsonReader file)
     file.path.clearArray(file.path.array, pathSize);
 
 }
-
-/*
-void mainA(JsonReader file)
-{
-    std::ifstream batchFile("../Test2.json", std::ifstream::binary);
-
-    // JSON-Datei in ein Json::Value-Objekt parsen
-    Json::Value root;
-    batchFile >> root;
-
-    // Datei schließen
-    batchFile.close();
-
-    if (batchFile.fail())
-    {
-        std::cerr << "Fehler beim Lesen der JSON-Datei." << std::endl;
-    }
-
-    if (root.isArray() && !root.empty())
-    {
-        Json::Value firstElement = root[0];
-
-        if (firstElement.isObject())
-        {
-            file.outputfileValue = firstElement["outputfile"];
-            file.hideshellValue = firstElement.get("hideshell", false).asBool();
-
-            if (!file.outputfileValue.isNull())
-            {
-                std::cout << "Wert von outputfile: " << file.outputfileValue.asString() << std::endl;
-                createBatchFile(file);
-            }
-            else
-            {
-                std::cerr << "Schlüssel 'outputfile' nicht gefunden im JSON." << std::endl;
-            }
-        }
-        else
-        {
-            std::cerr << "Das erste Element ist kein Objekt." << std::endl;
-        }
-    }
-    else
-    {
-        std::cerr << "Root ist kein Array oder ist leer." << std::endl;
-    }
-}
-*/
-/*
-ChatGPT
-Der Code hat einige Fehler und Unklarheiten, aber ich kann Ihnen helfen, diese zu korrigieren:
-
-Es gibt einen Syntaxfehler in der Zeile, die pointPosition zuweist. Es sollte name.find('.') sein, nicht name.find{"."}.
-Sie haben beforePoint nicht deklariert, bevor Sie versuchen, ihm einen Wert zuzuweisen. Sie müssen es als std::string deklarieren, bevor Sie es verwenden.
-Die Variable batchFile ist nur innerhalb der Schleife sichtbar, aber Sie verwenden sie außerhalb der Schleife. Sie sollten sicherstellen, dass batchFile im gesamten Gültigkeitsbereich verwendet werden kann.
-Die Variable fileName wird deklariert, aber nicht verwendet. Wenn Sie den Dateinamen verwenden möchten, stellen Sie sicher, dass Sie ihn irgendwo verwenden.
-Es gibt einige logische Probleme bei der Verwendung von beforePoint. Es scheint, dass Sie versuchen, den Dateinamen ohne Erweiterung zu bekommen. Wenn dies der Fall ist, sollten Sie den Code entsprechend anpassen.
-Hier ist der überarbeitete Code:
-
-cpp
-Copy code
-std::string name = file.outputfileValue.asString();
-int periodCount = 0;
-int nameLength = name.length();
-std::string fileName;
-
-for (int i = 0; i < nameLength; i++) {
-    if (name[i] == '.') {
-        size_t pointPosition = name.find('.');
-        std::string beforePoint = name.substr(0, pointPosition);
-        std::ofstream batchFile(beforePoint + ".bat", std::ios::binary);
-        fileName = beforePoint + ".bat";
-        break;
-    }
-    else if (i == nameLength - 1) {
-        std::ofstream batchFile(name + ".bat", std::ios::binary);
-        fileName = name + ".bat";
-    }
-    else {
-        periodCount++;
-    }
-}
-
-// Überprüfen, ob die Datei erfolgreich geöffnet wurde
-if (!batchFile.is_open())
-{
-    std::cerr << "Fehler beim Erstellen der Batch-Datei!" << std::endl;
-    return;
-}
-*/
--- testo.bat
+++ testo.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-C:\Windows\System32\cmd.exe /c "C:\Users\z0247951\AppData\Local\Programs\obsidian\Obsidian.exe && start "MinGW" C:\Users\z0247951\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+C:\Windows\System32\cmd.exe /c "C:\Users\z0247951\AppData\Local\Programs\obsidian\Obsidian.exe && start "testo.bat" C:\Users\z0247951\AppData\Local\Programs\Microsoft VS Code\Code.exe"
 @ECHO ON
